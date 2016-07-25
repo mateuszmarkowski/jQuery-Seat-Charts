960fe1176f127454b69047c535c977a4834ec86a
Jervis <cuijervis@126.com>
2016-07-25 19:22:38 +0800

    update find method
    update find id by regular expression, 
    now you can use find method like this:
    sc.find(/^1_.*/)
    this will return the id start with "1_" from sc

--- jquery.seat-charts.js
+++ jquery.seat-charts.js
@@ -512,39 +512,52 @@
 			
 				var seatSet = fn.set();
 			
-				//user searches just for a particual character
-				return query.length == 1 ? (function(character) {
-					fn.each(function() {
-						if (this.char() == character) {
-							seatSet.push(this.settings.id, this);
-						}
-					});
-					
-					return seatSet;
-				})(query) : (function() {
-					//user runs a more sophisticated query, so let's see if there's a dot
-					return query.indexOf('.') > -1 ? (function() {
-						//there's a dot which separates character and the status
-						var parts = query.split('.');
-						
-						fn.each(function(seatId) {
-							if (this.char() == parts[0] && this.status() == parts[1]) {
-								seatSet.push(this.settings.id, this);
-							}
-						});
-						
-						return seatSet;
-					})() : (function() {
-						fn.each(function() {
-
-							if (this.status() == query) {
-								seatSet.push(this.settings.id, this);
-							}
-						});
-						
-						return seatSet;
-					})();
-				})();
+				//is RegExp
+		                return query instanceof RegExp ?
+		                    (function () {
+		                        fn.each(function (id) {
+		                            if (id.match(query)) {
+		                                seatSet.push(id, this);
+		                            }
+		                        });
+		                        return seatSet;
+		                    })() :
+		                    (query.length == 1 ?
+		                            (function (character) {
+		                                //user searches just for a particual character
+		                                fn.each(function () {
+		                                    if (this.char() == character) {
+		                                        seatSet.push(this.settings.id, this);
+		                                    }
+		                                });
+		
+		                                return seatSet;
+		                            })(query) :
+		                            (function () {
+		                                //user runs a more sophisticated query, so let's see if there's a dot
+		                                return query.indexOf('.') > -1 ?
+		                                    (function () {
+		                                        //there's a dot which separates character and the status
+		                                        var parts = query.split('.');
+		
+		                                        fn.each(function (seatId) {
+		                                            if (this.char() == parts[0] && this.status() == parts[1]) {
+		                                                seatSet.push(this.settings.id, this);
+		                                            }
+		                                        });
+		
+		                                        return seatSet;
+		                                    })() :
+		                                    (function () {
+		                                        fn.each(function () {
+		                                            if (this.status() == query) {
+		                                                seatSet.push(this.settings.id, this);
+		                                            }
+		                                        });
+		                                        return seatSet;
+		                                    })();
+		                            })()
+		                    );
 				
 			},
 			set        : function set() {//inherits some methods
