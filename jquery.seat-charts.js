60c9b3886776319498b67e8409ec0eb266ca1fec
Sergej Karavajnij <basyanya@gmail.com>
2016-12-13 12:58:24 +0300

    add missing closing bracket

--- jquery.seat-charts.js
+++ jquery.seat-charts.js
@@ -427,7 +427,7 @@
 		//if there're any legend items to be rendered
 		settings.legend.items.length ? (function(legend) {
 			//either use user-defined container or create our own and insert it right after the seat chart div
-			var $container = (legend.node || $('<div></div').insertAfter(fn))
+			var $container = (legend.node || $('<div></div>').insertAfter(fn))
 				.addClass('seatCharts-legend');
 				
 			var $ul = $('<ul></ul>')
